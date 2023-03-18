02df8d53eda28631a3ff9609019bef163742f058
yole <igbavagodswill4@gmail.com>
2023-03-18 21:51:03 +0000

    made a lot of changes

--- script.js
+++ script.js
@@ -14,16 +14,72 @@ const annaNotification = document.querySelector('.anna');
 
 const users = [markNotification, angelaNotification, jacobNotification, rizkyNotification, 
 kimberlyNotification, nathanNotification, annaNotification];
+notificationNum.innerHTML = `${users.length}`;
+
+
 const message = document.querySelector('.message');
 
+message.classList.add('hidden');
+
+//for counting the notifications on the notification
+const notificationCount = function() {
+    const notifications = users.splice(1);
+    notificationNum.innerHTML = `${notifications.length}`
+};
+
 markAllBtn.addEventListener('click', function() {
+    markAllBtn.style.color = 'hsl(219, 85%, 26%)';
     users.forEach(function(el){
         el.classList.add('viewed');
         const notifications = users.splice();
         notificationNum.innerHTML = `${notifications.length}`
     });
-
-
 });
+
+
+markNotification.addEventListener('click', function() {
+    markNotification.classList.add('viewed');
+    notificationCount();
+});
+
+angelaNotification.addEventListener('click', function() {
+    angelaNotification.classList.add('viewed');
+    notificationCount();
+});
+
+jacobNotification.addEventListener('click', function() {
+    jacobNotification.classList.add('viewed');
+    notificationCount();
+});
+
+
+
+rizkyNotification.addEventListener('click', function() {
+    rizkyNotification.classList.add('viewed');
+    message.classList.remove('hidden');
+    notificationCount();
+});
+
+message.addEventListener('click', function() {
+    message.style.background = 'hsl(205, 33%, 90%)';
+    message.style.border = 'none';
+});
+
+kimberlyNotification.addEventListener('click', function() {
+    kimberlyNotification.classList.add('viewed');
+    notificationCount();
+});
+
+nathanNotification.addEventListener('click', function() {
+    nathanNotification.classList.add('viewed');
+    notificationCount();
+});
+
+annaNotification.addEventListener('click', function() {
+    annaNotification.classList.add('viewed');
+    notificationCount();
+});
+
+
 
 
