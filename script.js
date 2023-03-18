575a0af157241385111b1e6a01506316bfef3a4e
yole <igbavagodswill4@gmail.com>
2023-03-18 20:58:30 +0000

    made the mark all button functional

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 `use strict`;
 
+const markAllBtn = document.querySelector('.markAll');
+const notificationNum = document.querySelector('.notificationNum');
+
+//users
+const markNotification = document.querySelector('.mark');
+const angelaNotification = document.querySelector('.angela');
+const jacobNotification = document.querySelector('.jacob');
+const rizkyNotification = document.querySelector('.rizky');
+const kimberlyNotification = document.querySelector('.kimberly');
+const nathanNotification = document.querySelector('.nathan');
+const annaNotification = document.querySelector('.anna');
+
+const users = [markNotification, angelaNotification, jacobNotification, rizkyNotification, 
+kimberlyNotification, nathanNotification, annaNotification];
+const message = document.querySelector('.message');
+
+markAllBtn.addEventListener('click', function() {
+    users.forEach(function(el){
+        el.classList.add('viewed');
+        const notifications = users.splice();
+        notificationNum.innerHTML = `${notifications.length}`
+    });
+
+
+});
+
+
